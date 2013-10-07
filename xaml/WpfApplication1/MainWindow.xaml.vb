0d75e27952ab8ac6a497bd4569f955179400e699
Luis Vizcarra <l.vizcarra.delgado@isur.edu.pe>
2013-10-07 13:14:26 -0500

    notepad

---
+++ xaml/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,42 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+
+Class MainWindow
+
+    Private Sub nuevoClick(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MenuItem1.Click
+        document.Text = ""
+    End Sub
+
+    Private Sub guardarClick(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MenuItem1.Click
+        Dim save As New SaveFileDialog
+        Dim mistreamwrite As StreamWriter
+        save.Filter = "text (*.txt)|*.txt|html(*.html)|*.html|all files(*.*)|*.*"
+        save.CheckPathExists = True
+        save.Title = "guardar como"
+        save.ShowDialog(Me)
+        Try
+            mistreamwrite = File.AppendText(save.FileName)
+            mistreamwrite.Write(document.Text)
+            mistreamwrite.Flush()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub abrirClick(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MenuItem1.Click
+        Dim open As New OpenFileDialog
+        Dim mistreamwrite As StreamReader
+        open.Filter = "text (*.txt)|*.txt"
+        open.CheckFileExists = True
+        open.Title = "abrir"
+        open.ShowDialog()
+        Try
+            open.OpenFile()
+            mistreamwrite = New StreamReader((open.FileName))
+            document.Text = mistreamwrite.ReadToEnd
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
